a89b475968260eaf47b3f5e7f7156c5c667afee9
ToquiNovic <j.toquica@udla.edu.co>
2023-01-25 16:23:22 -0500

    logic

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import InSteps from "./components/LinealProgramming/InSteps";
+import LinealProgramming from './components/LinealProgramming'
+import SinglePage from "./components/LinealProgramming/SinglePage";
+
+const App = () => 
+    <Switch>
+      <Route exact path="/LinealProgramming" component={LinealProgramming} />
+      <Route exact path="/LinealProgramming/InSteps" component={InSteps} />
+      <Route exact path="/LinealProgramming/SinglePage" component={SinglePage} />
+      
+      <Redirect to="/LinealProgramming/SinglePage" />
+    </Switch>;
+
+export default App;
