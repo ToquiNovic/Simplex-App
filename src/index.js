47df1bdead349e880e868a122bf4939d82f39e1c
ToquiNovic <j.toquica@udla.edu.co>
2023-02-01 20:45:17 -0500

    PWA

--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ import './index.css';
 import 'react-vis/dist/style.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+
 
 
 ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById('root'));
 
-serviceWorker.unregister();
+serviceWorkerRegistration.register({
+    onUpdate: async (registration) => {
+      // Corremos este código si hay una nueva versión de nuestra app
+      // Detalles en: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+      if (registration && registration.waiting) {
+        await registration.unregister();
+        registration.waiting.postMessage({ type: "SKIP_WAITING" });
+        // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+        window.location.reload();
+      }
+    },
+  });
